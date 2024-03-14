f4ec93eddfc13e30c1eedaf5466a83a4a6b71650
Taidev2002 <tainguyendev02@gmail.com>
2024-03-14 17:32:29 +0700

    feat: navbar header

--- frontend/src/components/layout/Header/Header.jsx
+++ frontend/src/components/layout/Header/Header.jsx
@@ -1,29 +1,35 @@
-import React from "react";
-import Stack from '@mui/material/Stack';
-import { NavLink } from "react-router-dom";
-const className_isActive = 'bg-slate-300'
-const className_btn = 'text-black'
-const narbar = [{
-  label:'Hệ thống',
-  to:'/he-thong'
-}]
-const Header = () => {
+import React, { useState } from "react";
+import { Narbar } from "../../../data/header/hearderData";
+const className_isActive = " text-blue-500 border-b-2 border-blue-500";
+const className_btn = "text-black  text-center  px-2 py-4  font-medium hover:bg-sky-100 duration-300 cursor-pointer";
 
+const Header = () => {
+  const [isActive, setIsActive] = useState(null);
+  //xử lí click Navbar
+  const hanldClick = (label) => {
+    setIsActive(label);
+  };
   return (
-    <header className=" p-2 bg-[#fbfcfe] border border-transparent border-b-gray-300 ">
+    <header className=" bg-[#fbfcfe]  border border-transparent border-b-gray-300 ">
       <div className="flex">
-        <h2 className="bg-blue-500 font-medium text-white rounded-full text-base p-2">
+        <nav className="" >
+          <ul className="flex gap-2 h-full ">
+            <li className="py-2">
+            <h2 className="bg-blue-500 font-medium text-white rounded-full text-base p-2">
           315
         </h2>
-        <Stack direction="row" className="text-white">
-        {narbar?.map(items => <button>
-          <NavLink to={items.to} className={({isActive}) => 
-        isActive ? className_isActive:  className_btn
-        }>
-        {items.label}
-          </NavLink>
-        </button>)}
-        </Stack>
+            </li>
+            {Narbar?.map((items) => (
+              <li
+              onClick={()=>{hanldClick(items.label)}}
+              key={items.label}
+              className={isActive === items.label ? className_btn + className_isActive : className_btn  }
+              >
+                {items.label}
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
       <div></div>
     </header>
---
+++ frontend/src/data/header/hearderData.js
@@ -0,0 +1,44 @@
+export const Narbar = [
+    {
+      label: "Hệ thống",
+    },
+    {
+      label: "Danh mục",
+    },
+    {
+      label: "Khách hàng",
+    },
+    {
+      label: "Tiếp nhận",
+    },
+    {
+      label: "Phòng khám",
+    },
+    {
+      label: "Tiêm chủng",
+    },
+    {
+      label: "Cận lâm sàn",
+    },
+    {
+      label: "Nhà thuốc",
+    },
+    {
+      label: "Tài sản",
+    },
+    {
+      label: "Tài Chính",
+    },
+    {
+      label: "Quản trị",
+    },
+    {
+      label: "Báo cáo",
+    },
+    {
+      label: "Tiện ích",
+    },
+    {
+      label: "trợ giúp",
+    },
+  ];
