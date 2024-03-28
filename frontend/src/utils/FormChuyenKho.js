430700e5689efc885a986d2b4f4f4c4785f4cec7
Taidev2002 <tainguyendev02@gmail.com>
2024-03-28 17:21:24 +0700

    feat: printer page

---
+++ frontend/src/utils/FormChuyenKho.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FormChuyenKho = () => {
+  return (
+    <div  className="hidden">
+      HỆ THỐNG Y TẾ NHI ĐỒNG 315 26 Phan Văn Hớn, Xã Xuân Thới Thượng, Huyện Hóc
+      Môn, TP.HCM Điện thoại: 0937.821.315 - Hotline: 0901.315.315 Website:
+      https://www.nhidong315.com
+        <h2>Phiếu nhập Kho</h2>
+    </div>
+  );
+};
+
+export default FormChuyenKho;
