80a326e8eb6b438081c603ab0df7eba928dbcbbb
Taidev2002 <tainguyendev02@gmail.com>
2024-04-07 21:52:06 +0700

    feat: kham doan

--- frontend/src/components/layout/KhachKhamDoan/KhachKhamDoan.jsx
+++ frontend/src/components/layout/KhachKhamDoan/KhachKhamDoan.jsx
@@ -1,158 +1,149 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
+import Highlighter from 'react-highlight-words';
 import LayoutApp from "../../../HOCs/LayoutApp";
-import { Input, Button, Table, ConfigProvider, Tooltip, Tag,Popconfirm } from "antd";
+import {
+  Input,
+  Button,
+  Table,
+  ConfigProvider,
+  Tooltip,
+  Tag,
+  Popconfirm,
+  Space,
+  Modal
+} from "antd";
 import {
   DeleteOutlined,
   EditOutlined,
   ReloadOutlined,
   QuestionCircleOutlined,
+  SearchOutlined,
+  ExclamationCircleFilled
 } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import ModalAdd from "./ModalAddBenhNhan/ModalAdd";
 import * as typeAction from "../../../store/constants/constants";
-import ButtonImportExcel from "./ButtonImportExcel/ButtonImportExcel";
 import {
   deleteBNKhamDoanById,
   getAllBNKhamDoan,
   getListCtyKhamDoan,
   infoBNKhamDoanAction,
+  sendSMSKhamDoanAction,
 } from "../../../store/actions/khamDoanAction";
 import moment from "moment";
 import ModalImport from "./ModalInponrt/ModalImport";
 import ModalEditBenhNham from "./ModalEditBenhNhan/ModalEditBenhNham";
+const { confirm } = Modal;
 
-const columns = [
-  {
-    title: "Mã BN",
-    dataIndex: "MABN",
-    key: "MABN",
-    width: 100,
-  },
-  {
-    title: "Tên BN",
-    dataIndex: "TENBN",
-    key: "TEBN",
-    width: 200,
-  },
-  {
-    title: "Giới tính",
-    dataIndex: "GIOITINH",
-    key: "GIOITINH",
-    width: 80,
-    align: "center",
-  },
-  {
-    title: "Ngày sinh",
-    dataIndex: "NGAYSINH",
-    key: "NGAYSINH",
-    width: 100,
-    align: "center",
-  },
-  {
-    title: "SĐT",
-    dataIndex: "SODIENTHOAI",
-    key: "SODIENTHOAI",
-    width: 100,
-    align: "center",
-  },
-  {
-    title: "Trạng thái khám",
-    dataIndex: "TTKHAM",
-    key: "TTKHAM",
-    width: 180,
-  },
-  {
-    title: "Tên công ty",
-    dataIndex: "TENCTY",
-    key: "TENCTY",
-    width: 250,
-  },
-  {
-    title: "KQ xét Nghiệm",
-    dataIndex: "KQXN",
-    key: "KQXN",
-    width: 120,
-    align: "center",
-    filters: [
-      {
-        text: 'London',
-        value: 'London',
-      },
-      {
-        text: 'New York',
-        value: 'New York',
-      },
-    ],
-    onFilter: (value, record) => record.KQXN.indexOf(value) === 0,
-  },
-  {
-    title: "KQ khám",
-    dataIndex: "KQK",
-    key: "LQK",
-    width: 100,
-    align: "center",
-  },
-  {
-    title: "SMS",
-    dataIndex: "SMS",
-    key: "SMS",
-    width: 100,
-    align: "center",
-  },
-  {
-    title: "Người gửi SMS",
-    dataIndex: "NGUOIGUISNS",
-    key: "NGUOIGUISNS",
-    width: 150,
-  },
-  {
-    title: "Ngày gửi SMS",
-    dataIndex: "NGAYGUISMS",
-    key: "NGAYGUISMS",
-    width: 120,
-    align: "center",
-  },
-  {
-    title: "Người tạo",
-    dataIndex: "NGUOITAO",
-    key: "NGUOITAO",
-    width: 150,
-  },
-  {
-    title: "Ngày tạo",
-    dataIndex: "NGAYTAO",
-    key: "NGAYTAO",
-    width: 150,
-    align: "center",
-  },
-  {
-    title: "Ghi chú",
-    dataIndex: "GHICHU",
-    key: "GHICHU",
-    width: 200,
-  },
-  {
-    title: "Hành động",
-    dataIndex: "action",
-    key: "action",
-    align: "center",
-    width: 90,
-    fixed: "right",
-  },
-];
 const KhachKhamDoan = () => {
   const dispatch = useDispatch();
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-  const { listBNImport, ListBNKhamDoan } = useSelector(
+  const { listBNImport, ListBNKhamDoan,listSMS,isLoadingBN } = useSelector(
     (state) => state.khamDoanReducer
   );
-  const onSelectChange = (newSelectedRowKeys) => {
-    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+  const onSelectChange = (newSelectedRowKeys,infoBN) => {
+   dispatch({
+    type:typeAction.ADD_LIST_INFO_SMS,
+    payload:infoBN
+   })
     setSelectedRowKeys(newSelectedRowKeys);
   };
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={'Nhập tìm kiếm'}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Tìm
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Xoá
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            Đóng
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1677ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
   const handlDeleteBenhNhan = (idbn) => {
-    dispatch(deleteBNKhamDoanById(idbn))
-  }
+    dispatch(deleteBNKhamDoanById(idbn));
+  };
   const handleAddKhamDoan = () => {
     dispatch({
       type: typeAction.OPEN_ADD_KHAM_DOAN,
@@ -162,10 +153,194 @@ const KhachKhamDoan = () => {
     selectedRowKeys,
     onChange: onSelectChange,
   };
+  const showConfirm = () => {
+    confirm({
+      title: 'Bạn có muốn gửi sms hay không ?',
+      icon: <ExclamationCircleFilled />,
+      content: `số lượng bệnh nhân gửi của bạn là ${listSMS.length} bệnh nhân !`,
+      okText:'Gửi SMS',
+      cancelText:'Huỷ bỏ',
+      onOk() {
+       dispatch(sendSMSKhamDoanAction(listSMS))
+       setSelectedRowKeys([]);
+      },
+      onCancel() {
+        setSelectedRowKeys([]);
+      },
+    });
+  };
   useEffect(() => {
     dispatch(getAllBNKhamDoan());
     dispatch(getListCtyKhamDoan());
   }, []);
+  useEffect(()=>{
+    console.log(listSMS);
+  },[listSMS])
+  const columns = [
+    {
+      title: "Mã BN",
+      dataIndex: "MABN",
+      key: "MABN",
+      width: 100,
+      ...getColumnSearchProps('MABN'),
+    },
+    {
+      title: "Tên BN",
+      dataIndex: "TENBN",
+      key: "TENBN",
+      width: 200,
+      ...getColumnSearchProps('TENBN'),
+    },
+    {
+      title: "Giới tính",
+      dataIndex: "GIOITINH",
+      key: "GIOITINH",
+      width: 90,
+      align: "center",
+      ...getColumnSearchProps('GIOITINH'),
+    },
+    {
+      title: "Ngày sinh",
+      dataIndex: "NGAYSINH",
+      key: "NGAYSINH",
+      width: 100,
+      align: "center",
+    },
+    {
+      title: "SĐT",
+      dataIndex: "SODIENTHOAI",
+      key: "SODIENTHOAI",
+      width: 100,
+      align: "center",
+      ...getColumnSearchProps('SODIENTHOAI'),
+    },
+    {
+      title: "Trạng thái khám",
+      dataIndex: "TTKHAM",
+      key: "TTKHAM",
+      width: 180,
+    },
+    {
+      title: "Tên công ty",
+      dataIndex: "TENCTY",
+      key: "TENCTY",
+      width: 250,
+      ...getColumnSearchProps('TENCTY'),
+    },
+    {
+      title: "KQ xét Nghiệm",
+      dataIndex: "KQXN",
+      key: "KQXN",
+      width: 130,
+      align: "center",
+      filters: [
+        {
+          text: "Chưa có",
+          value: "Chưa có",
+        },
+        {
+          text: "Hoàn thành",
+          value: "Hoàn thành",
+        },
+      ],
+      onFilter: (value, record) =>
+        record.KQXN.props.children.indexOf(value) === 0,
+    },
+    {
+      title: "KQ khám",
+      dataIndex: "KQK",
+      key: "LQK",
+      width: 100,
+      align: "center",
+      filters: [
+        {
+          text: "Chưa có",
+          value: "Chưa có",
+        },
+        {
+          text: "Hoàn thành",
+          value: "Hoàn thành",
+        },
+      ],
+      onFilter: (value, record) =>
+        record.KQXN.props.children.indexOf(value) === 0,
+    },    {
+      title: "Người cập nhập KQ",
+      dataIndex: "NGUOIKQ",
+      key: "NGUOIKQ",
+      width: 150,
+      align: "center",
+
+    },    {
+      title: "Ngày cập nhập KQ",
+      dataIndex: "NGAYKQ",
+      key: "NGAYKQ",
+      width: 150,
+      align: "center",
+    },
+    {
+      title: "SMS",
+      dataIndex: "SMS",
+      key: "SMS",
+      width: 120,
+      align: "center",
+      filters: [
+        {
+          text: "Chưa gửi",
+          value: "Chưa gửi",
+        },
+        {
+          text: "Gửi thành công",
+          value: "Gửi thành công",
+        },
+        {
+          text: "Gửi thất bại",
+          value: "Gửi thất bại",
+        },
+      ],
+      onFilter: (value, record) => record.SMS.props.children.indexOf(value) === 0,
+    },
+    {
+      title: "Người gửi SMS",
+      dataIndex: "NGUOIGUISNS",
+      key: "NGUOIGUISNS",
+      width: 150,
+    },
+    {
+      title: "Ngày gửi SMS",
+      dataIndex: "NGAYGUISMS",
+      key: "NGAYGUISMS",
+      width: 120,
+      align: "center",
+    },
+    {
+      title: "Người tạo",
+      dataIndex: "NGUOITAO",
+      key: "NGUOITAO",
+      width: 150,
+    },
+    {
+      title: "Ngày tạo",
+      dataIndex: "NGAYTAO",
+      key: "NGAYTAO",
+      width: 150,
+      align: "center",
+    },
+    {
+      title: "Ghi chú",
+      dataIndex: "GHICHU",
+      key: "GHICHU",
+      width: 200,
+    },
+    {
+      title: "Hành động",
+      dataIndex: "action",
+      key: "action",
+      align: "center",
+      width: 90,
+      fixed: "right",
+    },
+  ];
   return (
     <LayoutApp>
       <div className="p-2">
@@ -186,9 +361,11 @@ const KhachKhamDoan = () => {
               Tải lại
             </Button>
           </div>
-          <div className="flex gap-5">
+          <div className="flex items-center gap-5">
+          {selectedRowKeys.length !== 0 ? <p>Đã chọn {selectedRowKeys.length} </p> :'' }
             <Button
-              onClick={handleAddKhamDoan}
+            disabled={selectedRowKeys.length !== 0 ? false : true}
+              onClick={showConfirm}
               className="bg-orange-600 hover:bg-orange-500 text-white"
             >
               Gửi SMS
@@ -220,6 +397,7 @@ const KhachKhamDoan = () => {
             }}
           >
             <Table
+              loading={isLoadingBN}
               pagination={false}
               bordered
               rowSelection={rowSelection}
@@ -244,6 +422,8 @@ const KhachKhamDoan = () => {
                   kqxn,
                   kqkham,
                   daguisms,
+                  ngaykq,
+                  nguoikq,
                   ngaytao,
                   nguoitao,
                   ngayguisms,
@@ -253,7 +433,7 @@ const KhachKhamDoan = () => {
                   MABN: mabn,
                   TENBN: tenbn,
                   GIOITINH: gioitinh,
-                  NGAYSINH: moment(ngaysinh).format("DD/MM/YYYY"),
+                  NGAYSINH: ngaysinh && moment(ngaysinh).format("DD/MM/YYYY"),
                   SODIENTHOAI: sodienthoai,
                   TTKHAM: trangthai,
                   KQXN: !kqxn ? (
@@ -267,11 +447,13 @@ const KhachKhamDoan = () => {
                     <Tag color="green">Hoàn thành</Tag>
                   ),
                   TENCTY: tenct,
-                  NGUOIGUISNS:nguoiguisms,
-                  NGAYGUISMS:ngayguisms,
-                  NGUOITAO:nguoitao,
-                  NGAYTAO:moment(ngaytao).format('DD/MM/YY hh:mm:ss'),
-                  GHICHU:ghichu,
+                  NGUOIGUISNS: nguoiguisms,
+                  NGAYGUISMS: ngayguisms,
+                  NGUOITAO: nguoitao,
+                  NGAYTAO: ngaytao && moment(ngaytao).format("DD/MM/YY hh:mm:ss"),
+                  NGAYKQ: ngaykq && moment(ngaykq).format("DD/MM/YY hh:mm:ss"),
+                  NGUOIKQ:nguoikq,
+                  GHICHU: ghichu,
                   SMS:
                     trangthaisms === 1 ? (
                       <Tag>Chưa gửi</Tag>
@@ -307,16 +489,19 @@ const KhachKhamDoan = () => {
                               />
                             }
                           >
-                          <DeleteOutlined />
+                            <DeleteOutlined />
                           </Popconfirm>
                         </Tooltip>
                       </li>
-                      <li onClick={()=>{
-                        dispatch(infoBNKhamDoanAction(idbn))
-                        dispatch({
-                          type:typeAction.OPEN_MODAL_EDIT_BN_KHAM_DOAN
-                        })
-                      }} className="text-lg text-green-500">
+                      <li
+                        onClick={() => {
+                          dispatch(infoBNKhamDoanAction(idbn));
+                          dispatch({
+                            type: typeAction.OPEN_MODAL_EDIT_BN_KHAM_DOAN,
+                          });
+                        }}
+                        className="text-lg text-green-500"
+                      >
                         <Tooltip
                           title="sửa"
                           className="cursor-pointer"
@@ -337,7 +522,7 @@ const KhachKhamDoan = () => {
       </div>
       <ModalAdd />
       <ModalImport />
-      <ModalEditBenhNham/>
+      <ModalEditBenhNham />
     </LayoutApp>
   );
 };
--- frontend/src/schemas/editCtyKhamDoanSchema.js
+++ frontend/src/schemas/editCtyKhamDoanSchema.js
@@ -1,11 +1,10 @@
 import * as Yup from "yup";
 
-export const addCtyKhamDoanSchema = Yup.object().shape({
+export const editCtyKhamDoanSchema = Yup.object().shape({
     tenct: Yup.string().required(),
     diachi: Yup.string().required(),
     dienthoai: Yup.string().required(),
     fax: Yup.string().required(),
     email: Yup.string().required(),
-    mact: Yup.string().required(),
     // website: Yup.string().required(),
 });
